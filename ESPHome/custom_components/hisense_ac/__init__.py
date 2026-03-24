165d0a1abf1f5a94dbacb7a6e4f0fd5e1d111c07
Adam Kiss <kissadam12g@gmail.com>
2026-03-24 20:34:17 +0100

    renamed folder

--- ESPHome/custom_components/hisense_ac/__init__.py
+++
@@ -1,7 +0,0 @@
-import esphome.codegen as cg
-from esphome.components import uart, climate
-
-DEPENDENCIES = ["uart"]
-
-hisense_ac_ns = cg.esphome_ns.namespace("hisense_ac")
-HisenseAC = hisense_ac_ns.class_("HisenseAC", uart.UARTDevice, cg.Component, climate.Climate)
